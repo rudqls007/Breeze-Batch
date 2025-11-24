7d85b7ac647e1f04c62b0952cb901807fa855f79
kblee <rudqls007@gmail.com>
2025-11-24 18:10:30 +0900

    Initial commit

---
+++ src/main/java/com/example/kybatch/KyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kybatch/KyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
